950afce5063d3e06bd0c606294a6500a05942b58
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-06-24 14:24:54 +0100

    Pushing Game of life code - exception class

---
+++ src/IntputLimitExceededException.java
@@ -0,0 +1,11 @@
+
+public class IntputLimitExceededException extends Exception 
+{
+
+	private static final long serialVersionUID = 1L;
+
+	public IntputLimitExceededException(String message)
+	{
+		super(message);
+	}
+}
