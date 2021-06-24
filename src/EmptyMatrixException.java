590290ad08be24c864b2e194e0bd520b5b2e307e
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-06-24 14:23:27 +0100

    Pushing Game of life code - exception class

---
+++ src/EmptyMatrixException.java
@@ -0,0 +1,11 @@
+
+public class EmptyMatrixException extends Exception 
+{
+
+	private static final long serialVersionUID = 1L;
+
+	public EmptyMatrixException(String message)
+	{
+		super(message);
+	}
+}
